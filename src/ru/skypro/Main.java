048bc3ed83d88c66ec92511bb0cf7d11e46aeefc
ved7997 <ved7997@gmail.com>
2021-12-30 12:01:55 +0300

    Вторая попытка

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        //pervoe_DZ();
-        //vtoroe_DZ();
-        trete_DZ();
+        pervoe_task();
+        vtoroe_task();
+        trete_task();
     }
-    public static void pervoe_DZ() {
+    public static void pervoe_task() {
 
         int j = 1;
         for (int i = 0; i < 10; i++) {
@@ -23,17 +23,20 @@ public class Main {
             System.out.print(jj + " ");
             jj = jj - 1;
         }
-
+        System.out.println();
+        System.out.println();
     }
-    public static void vtoroe_DZ(){
+    public static void vtoroe_task(){
         int pytnyca = 3;
         for (int i = 0;i <=31;i = i + 7) {
 
             System.out.println("Сегодня пятница, " + pytnyca + " -е число. Необходимо подготовить отчет.");
             pytnyca = pytnyca + 7;
+
         }
+        System.out.println();
     }
-    public static void trete_DZ(){
+    public static void trete_task(){
         int god = (2017 + 79)- 200;
         for (int i = god;i <=2100;i = i + 79){
             System.out.println(i + "г");
